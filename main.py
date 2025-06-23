17d182b6d54bbe9820f6df85cf4c5f78c6748fca
Manish Prajapati <marveluniverse1942@gmail.com>
2025-06-23 12:10:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-a=10
-b=20
-sum=a+b
-
-print("The Sum Of",a,"and",b,"is = ",sum)
-print(str(b)+ " is greater than " +str(a))
-print(b,"is greater than",a)
+print("Hello Python!")
+print("This is my python tutorial repository")
